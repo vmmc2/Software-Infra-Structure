b4676d0d06fd21ed0325371f1bd8a7bac1d29236
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-03-07 16:32:00 -0300

    Update q2.asm

--- q2.asm
+++ q2.asm
@@ -1,6 +1,8 @@
 org 0x7c00 
 jmp 0x0000:start
 
+variavel db 1
+
 entrada db 'Por favor digite um numero que sera a base', 13, 10, 0 ;reservando espaço na memoria para a string
 entrada2 db 'Digite o numero que servira como expoente', 13, 10, 0
 
@@ -13,31 +15,38 @@ start:
     mov es, ax
     mov si, entrada ; faz com que o registrador si aponte para o inicio da string de nome entrada
     call print1 ;funcao responsavel por printar string entrada (chamada de função)
-    call scanfbase ;funcao para ler o primeiro numero (base)
+    xor si, si ; limpando agora oq estava presente no registrador si
+    call scanfbase ; funcao para ler o primeiro numero (base)
+start2:
+    call _endl ;pulando uma linha pra poder printar a entrada 2    
+    mov si, entrada2
+    call print1
+    call scanfbase
     jmp done
 
-
 print1:
-    lodsb
+    lodsb; sempre carrega algo do registrador si ou di no registrador al.
     cmp al, 0
     je .done
-
-    mov ah, 0eh
+    mov ah, 0eh; interrupcao para imprimir um caractere
     int 10h
-    jmp print1
+    jmp print1; volta pra print1 pra imprimir o prox caractere
     .done:
         ret
 
 print:
-    lodsb ; carrega uma letra da string no registrador al e passa para o prox caractere
-    cmp cl, 0 ;checa pra ver se eu cheguei ao fina de uma string. toda string é delimitafa pelo caractere \0
-    je done ;se tiver chegado, cabou-se  ---- je == jump if equal
-	
-    ;agora eu tenho que printar o que foi carregado no meu registrador al (no caso, o caractere que foi carregado no registrador al)
-    ;chama uma interrupcao pra printar o caractere
-	dec cl
-    call putchar
-    jmp print
+    call _endl
+    .ok:
+        lodsb ; carrega uma letra da string no registrador al e passa para o prox caractere
+        mov byte[variavel], al
+        cmp cl, 0 ;checa pra ver se eu cheguei ao fina de uma string. toda string é delimitafa pelo caractere \0
+        je start2 ;se tiver chegado, cabou-se  ---- je == jump if equal
+        ;agora eu tenho que printar o que foi carregado no meu registrador al (no caso, o caractere que foi carregado no registrador al)
+        ;chama uma interrupcao pra printar o caractere
+        dec cl
+        call putchar
+    jmp .ok
+    
 
 getchar: ; funcao para ler um caractere por vez
     mov ah, 0
@@ -60,8 +69,15 @@ scanfbase: ; funcao maior responsavel por ler uma string. na verdade ela eh um l
     call putchar ; toda vez que eu ler um caractere, eu tenho que printa-lo
     jmp scanfbase  
 
+_endl:
+    mov al, 0x0a
+    call putchar
+    mov al, 0x0d
+    call putchar
+    ret
+
 done:
     jmp $
-times 510-($-$$) db 0		; preenche o resto do setor com zeros 
-dw 0xaa55			; coloca a assinatura de boot no final
+    times 510-($-$$) db 0		; preenche o resto do setor com zeros 
+    dw 0xaa55			; coloca a assinatura de boot no final
 				; do setor (x86 : little endian)
