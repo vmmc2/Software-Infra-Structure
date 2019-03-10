c44964903e7b5f0d8dd7e7a1acd7a9a93a639798
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-03-10 03:53:21 +0000

    Update newQ2.asm

--- newQ2.asm
+++ newQ2.asm
@@ -1,48 +1,97 @@
 org 0x7c00 
 jmp 0x0000:start
 
+
 start:
-    xor di, di
-    xor si, si
-	xor ax, ax ;zerando o conteudo de cada um dos registradores. xor == mais rapido
-	mov ds, ax
+    base dw 0
+    xor ax, ax ; zerando o conteudo do registrador ax
+    mov ds, ax
     mov es, ax
-    ;---------Passo 1: Ler o numero que vai ser a base da potenciacao-------------------------------------
-	call getchar ;teoricamente, como a gente vai digitar so um caractere (um digito) da pra ignorar o ent
-    ;do usuario
-    ;o numero ficou salvo no registrador AL como um caractere.
-    ;Exemplo: Se eu digitei 3, no registrador AL eu tenho guardado: '3'
-    mov bl, al ; To movendo a base da exponenciacao para o registrador BX
-    call _endl ;pulei uma linha e coloquei o cursor no inicio
-    call putchar ;printei o caractere que tava em AL (a base)
-    call _endl ;pulei outra linha
-    ;---------Passo 2: Ler o numero que vai ser o expoente da potenciacao-------------------------------------
-    call getchar
-    mov cl, al; To movendo o expoente da potenciacao para o registrador CX
-    call _endl
-    call putchar
-    call _endl
-    jmp done
+    call get_string
+    call printstring
+    xor si, si
+    xor di, di
+    xor ax, ax
+    call dale
+    mov di, word[base]
+    mov si, di
+    call printstring
+    call dale
+    xor si, si
+    xor di, di
+    xor ax, ax
+    call get_string
+    call printstring
+    
+ 
 
-getchar:
-    mov ah, 0x00
+printstring:
+    mov word[base], di
+    .loop:
+        lodsb ; bota o caractere em AL
+        cmp al, 0 ; se tiver chegado ao final da string acabou
+        je .endloop
+
+        mov ah, 0x0e
+        int 10h
+        jmp .loop
+    .endloop:
+        ret
+
+get_string:
+    xor cl, cl ;limpa o registrador de contagem
+    .loop:
+        mov ah, 0
+        int 16h ;interrrupcao para poder ler o caractere
+
+        cmp al, 0x08 ; compara pra ver se foi o backspace que foi pressionado
+        je .backspace
+
+
+        cmp al, 0x0d ; compara pra ver se foi o enter que foi pressionado
+        ; se sim, entao acabou a leitura
+        je dale
+
+        mov ah, 0x0e ; funcao pra printar o caractere pressionado
+        int 10h
+
+        stosb ; carrega o caracter que estava em AL para o registrador DI
+        inc cl
+        jmp .loop
+
+.backspace:
+    cmp cl, 0 ; se eu tiver no inicio da string eu ignoro o enter que foi pressionado
+    je .loop  ; e volto pra leitura de dados
+    
+    dec di
+    mov byte[di], 0
+    dec cl
+
+    mov ah, 0x0e
+    mov al, 0x08
     int 10h
+
+    mov al, ' '
+    int 10h
+
+    mov al, 0x08
+    int 10h
+
+    jmp .loop
+
+dale:
+    mov al, 0
+    stosb
+
+    mov ah, 0x0e
+    mov al, 0x0d
+    int 0x10
+    mov al, 0x0a
+    int 0x10
+
     ret
 
-_endl:
-    mov al, 0x0a ; 0x0a == caractere de mudanca de linha '\n' pula pra prox linha e mantem a coluna
-    call putchar
-    mov al, 0x0d ; 0x0d == caractere '\r' pula para a coluna inicial.
-    ; dps disso o cursor vai estar na margem esquerda da tela.
-    call putchar
-    ret
 
-putchar: ; a funcao putchar vai printar o caractere que estiver salvo no registrador AL
-	mov ah, 0x0e
-	int 10h
-	ret
-
-	
 done:
     jmp $
     times 510-($-$$) db 0	
