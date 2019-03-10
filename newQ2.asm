69b08dd239cf27fbf5556ca6825b01f88a544a61
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-03-10 15:21:49 +0000

    Update newQ2.asm

--- newQ2.asm
+++ newQ2.asm
@@ -1,38 +1,44 @@
 org 0x7c00 
 jmp 0x0000:start
 
+data:
+    base times 11 db 0
+	expoente times 11 db 0
 
 start:
-    base dw 0
+    mov di, base
     xor ax, ax ; zerando o conteudo do registrador ax
     mov ds, ax
     mov es, ax
     call get_string
-    call printstring
-    
-    xor si, si
-    xor di, di
+	call dale
+    mov si, base
     xor ax, ax
-    call dale ; funcao q pula linha
     
     
-    ; TA DANDO MERDA AQUI
-    mov di, word[base]
-    mov si, di
+    ; ISSO AQUI SO SERVE PRA PRINTAR A BASE QUE EU ARMAZENEI
+    mov si, base
     call printstring
     call dale
     
-    ; A PARTIR DAQUI VOLTA A PEGAR LEGAL
+    ; ISSO AQUI SERVE PARA LER O EXPOENTE E PRINTAR ELE NA LINHA DEBAIXO
+	xor di, di
+	mov di, expoente
     xor si, si
-    xor di, di
     xor ax, ax
     call get_string
-    call printstring
+    call dale
+
+	; ISSO AQUI VAI SERVIR PARA PRINTAR O EXPOENTE QUE EU ARMAZENEI
+	mov si, expoente
+	call printstring
+	call dale
+
+	;PRONTO TANTO BASE COMO EXPOENTE SALVOU OS DADOS DIREITINHO -- gloria
     
  
 
 printstring:
-    mov word[base], di
     .loop:
         lodsb ; bota o caractere em AL
         cmp al, 0 ; se tiver chegado ao final da string acabou
@@ -56,7 +62,7 @@ get_string:
 
         cmp al, 0x0d ; compara pra ver se foi o enter que foi pressionado
         ; se sim, entao acabou a leitura
-        je dale
+        je .endloop
 
         mov ah, 0x0e ; funcao pra printar o caractere pressionado
         int 10h
@@ -64,6 +70,10 @@ get_string:
         stosb ; carrega o caracter que estava em AL para o registrador DI
         inc cl
         jmp .loop
+    .endloop:
+        mov al, 0
+        stosb 
+        ret
 
 .backspace:
     cmp cl, 0 ; se eu tiver no inicio da string eu ignoro o enter que foi pressionado
